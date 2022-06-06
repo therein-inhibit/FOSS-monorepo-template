c3c4a75b4a1536c772774f458b5512b05c073935
MGJ <mgj@MGJs-M1-MacBook-Air.local>
2022-06-06 20:20:15 -0300

    added todo

--- ui-system/src/App.tsx
+++ ui-system/src/App.tsx
@@ -1,5 +1,6 @@
 import type { Component } from 'solid-js';
 
+// TODO: create containers, elements, components and widget
 const App: Component = () => {
   return (
     <p class="text-4xl text-green-700 text-center py-20">Hello tailwind ui-system!</p>
