a285af70be0efc9d02e9f4b6693afdf0673ae9af
MGJ <mgj@MGJs-M1-MacBook-Air.local>
2022-06-12 19:09:17 -0300

    basic center container done

--- ui-system/src/App.tsx
+++ ui-system/src/App.tsx
@@ -1,9 +1,19 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import Box from "ui-system/containers/Box";
+
+import Center from "ui-system/containers/Center";
+// import './index.css'
 
 // TODO: create containers, elements, components and widget
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind ui-system!</p>
+    // <Center class="">
+    //   <Box class="h-28 w-28 bg-yellow-200">A</Box>
+    // </Center>
+    <Center class=" h-screen w-screen bg-green-200">
+      <Box class="h-28 w-28 bg-yellow-200"></Box>
+    </Center>
+    // <p class="text-4xl text-green-700 text-center py-20"></p>
   );
 };
 
--- ui-system/tailwind.config.js
+++ ui-system/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+    './{src,containers}/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
   ],
   darkMode: 'class',
   theme: {
