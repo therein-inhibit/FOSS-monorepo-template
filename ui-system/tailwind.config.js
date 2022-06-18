da2bafdddc84328739b02ecc51481397b4da78bc
MGJ <mgj@MGJs-M1-MacBook-Air.local>
2022-06-18 14:59:39 -0300

    added all tailwind styles, execpt in production

--- ui-system/tailwind.config.js
+++ ui-system/tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./index.html",
     "./{src,containers,elements,components,widgets}/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
   ],
+
+  // NOTE: the reason to add all tailwind is to allow a
+  // user to edit a storybook using tailwind classes
   safelist: isProd ? [] : [{ pattern: /.*/ }],
   darkMode: "class",
   theme: {
