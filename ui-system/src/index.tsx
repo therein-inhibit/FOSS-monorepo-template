24d19642d822d65f7bbb3e5d256362dbd28376b6
MGJ <mgj@MGJs-M1-MacBook-Air.local>
2022-06-24 14:18:29 -0300

    using less shades of gray

--- ui-system/src/index.tsx
+++ ui-system/src/index.tsx
@@ -5,3 +5,39 @@ import { render } from "solid-js/web";
 import App from "./App";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
+
+// import { range } from "lib";
+
+// let cache = [];
+
+// for (let i of range(0, 255, 5)) {
+//   for (let j of range(0, 100, 5)) {
+//     let color = String(i);
+//     let opacity = String(j);
+
+//     if (i < 10) {
+//       color = `00${i}`;
+//     } else if (i >= 10 && i < 100) {
+//       color = `0${i}`;
+//     }
+
+//     if (j < 10) {
+//       opacity = `00${j}`;
+//     } else if (j >= 10 && j < 100) {
+//       opacity = `0${j}`;
+//     }
+
+//     // console.log(
+//     //   `"${color}-${opacity}": "rgba(${color},${color},${color}, ${j / 100})"`
+//     // );
+//     if (j === 100) {
+//       cache.push(`"${color}": "rgba(${color},${color},${color}, ${j / 100})"`);
+//     } else {
+//       cache.push(
+//         `"${color}-${opacity}": "rgba(${color},${color},${color}, ${j / 100})"`
+//       );
+//     }
+//   }
+// }
+
+// console.log(cache.join(",\n"));
