7d1b2d5794a6e5e34b3cee0c4f7f1ba5defe2fd4
MGJ <mgj@MGJs-M1-MacBook-Air.local>
2022-06-24 17:54:45 -0300

    saving progrees .need to refactor things

--- ui-system/elements/Loader/Loader.tsx
+++ ui-system/elements/Loader/Loader.tsx
@@ -21,7 +21,7 @@ export function Loader({ children, class: klass = "", ...rest }: LoaderProps) {
           {/* <span class={styles.CircleButtonAnimationFast}></span> */}
           {/* <Center class={`${styles.Pulse} ${styles.CircleButtonRaised}`}> */}
           <Center class={`${styles.CircleButtonRaised}`}>
-            {/* <svg
+            <svg
               xmlns="http://www.w3.org/2000/svg"
               class={`h-10 w-10 rounded-full ${styles.Spin} `}
               aria-hidden="true"
@@ -33,13 +33,13 @@ export function Loader({ children, class: klass = "", ...rest }: LoaderProps) {
               <path
                 fill="none"
                 // stroke="currentColor"
-                stroke="var(--app-gray-150)"
+                stroke="var(--app-gray-110)"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="4"
                 d="M24 4v4m10-1.32l-2 3.464M41.32 14l-3.464 2M44 24h-4m1.32 10l-3.464-2M34 41.32l-2-3.464M24 44v-4m-10 1.32l2-3.464M6.68 34l3.464-2M4 24h4M6.68 14l3.464 2M14 6.68l2 3.464"
               />
-            </svg> */}
+            </svg>
 
             {/* <Center> */}
             {/* <svg
@@ -76,8 +76,8 @@ export function Loader({ children, class: klass = "", ...rest }: LoaderProps) {
               />
             </svg> */}
 
-            <svg
-              class={`h-12 w-12 rounded-full ${styles.Spin} `}
+            {/* <svg
+              class={`h-10 w-10 rounded-full ${styles.Spin} `}
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
               width="1em"
@@ -94,7 +94,7 @@ export function Loader({ children, class: klass = "", ...rest }: LoaderProps) {
                 stroke-width="4"
                 d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Zm0-32v3m8.485.515l-2.121 2.121M36 24h-3m-.515 8.485l-2.121-2.121M24 36v-3m-8.485-.515l2.121-2.121M12 24h3m.515-8.485l2.121 2.121"
               />
-            </svg>
+            </svg> */}
             {/* </Center> */}
           </Center>
         </Center>
